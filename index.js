24129a1e447f55c1aed3d6b10399f803cb9072e3
Jörn Röder <kontakt@joernroeder.de>
2016-10-25 13:04:28 +0200

    added public statics

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = require('http').createServer(app);
 const port = process.env.PORT || 3000;
 const io = require('socket.io')(server);
 
+app.use(express.static('public'));
+
 // client html & javascript
 app.get('/', function (reg, res) {
   res.sendFile(__dirname + '/client.html');
